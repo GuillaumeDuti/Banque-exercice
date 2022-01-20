d10ccd928ad966da78e491832e2db46bb4ebad67
GuillaumeDuti <dutilleuxguillaume@gmail.com>
2022-01-20 12:03:54 +0100

    setup models banque

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 import { Personne } from './models/Personne.js';
 import { Compte } from './models/Compte.js';
+import { Banque } from './models/Banque.js';
 
 let user = new Personne("Dut", "Gui", "31 aout 1993");
-let compte = new Compte(23, 400, 200, user);
+let user2 = new Personne("Dutu", "Gui", "31 aout 1993");
+let compte = new Compte("BE25123456789", 400, 200, user);
+let compte2 = new Compte("BE25123456782", 400, 200, user2);
+let banque = new Banque();
 
 console.log(compte);
 
 window.user = user;
+window.user2 = user2;
 window.compte = compte;
+window.compte2 = compte2;
+window.banque = banque;
--- models/Banque.js
+++ models/Banque.js
@@ -1,7 +1,35 @@
-class Banque {
-    nom = "Newnew banquebanque";
-    comptes = [];
-    constructor() {}
+import { Compte } from './Compte.js';
+import { Personne } from './Personne.js';
 
+export class Banque {
+    nom = "Newnew banquebanque";
+    #comptes;
+    constructor() {
+        this.#comptes= new Map();
+    }
+
+    avoirDesComptes(titulaire) {
+        if(titulaire.constructor !== Personne.prototype.constructor) throw new TypeError('titulaire is not a Personne type');
+        let sommeDesAvoirs = 0;
+        // for(const [key, compte] of this.#comptes) {
+        //     if(compte.Titulaire === titulaire && compte.Solde > 0) sommeDesAvoirs += compte.Solde;
+        //     else return "no account";
+        // }
+        for(const compte of this.#comptes.values()) {
+            if(compte.Titulaire === titulaire && compte.Solde > 0) sommeDesAvoirs += compte.Solde;
+        }
+        return sommeDesAvoirs;
+    }
+
+    ajouterCompte(newCompte) {
+        if(newCompte.constructor !== Compte.prototype.constructor) throw new TypeError('newCompte is not a Compte type')
+        if(this.#comptes.has(newCompte.numero)) throw new RangeError('account already setup')
+        this.#comptes.set(newCompte.numero, newCompte);
+    }
+    
+    compte(numero) {
+        if(!this.#comptes.has(numero)) throw new RangeError('no such account')
+        return this.#comptes.get(numero)
+    }
 
 }
